8415fc2c5cdf469495561e1b65db74acb990b188
qa_kdy <qa_kdy@mail.ru>
2022-04-21 14:00:44 +0300

    add all files

---
+++ encrypto_tests/wdio.config.js
@@ -0,0 +1,25 @@
+exports.config = {
+    runner: 'local',
+    port: 4723,
+    host: 'localhost',
+    path: '/wd/hub',
+    logLevel: 'info',
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        require: ['@babel/register'],
+        timeout: 600000
+    },
+    maxInstances: 1,
+    sync: true,
+    specs: [
+        './tests/specs/**/*.js'
+    ],
+    capabilities:[{
+        "platformName": "Android",
+        "appium:automationName": "UiAutomator2",
+        "appium:deviceName": "emulator-5554",
+        "appium:appPackage": "com.encryptoportfolio.app",
+        "appium:appActivity": ".MainActivity"
+    }]
+}
